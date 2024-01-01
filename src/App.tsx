6c669f138e8cdee7e4d7abf245d1291a1fdf1ba6
Ankit Sharma <ankitsharma290101@gmail.com>
2024-01-01 21:08:01 +0530

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/signin" element={<SignInForm />} />
+        {/* Private Routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 };
 
