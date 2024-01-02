626aec4e72ab8677b7d38dc283681125a6aafa6e
Ankit Sharma <ankitsharma290101@gmail.com>
2024-01-02 21:38:18 +0530

    Folders+ShadCnUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import "./index.css";
+import "./globals.css";
 import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* Public Routes */}
-        <Route path="/signin" element={<SignInForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SigninForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Route>
         {/* Private Routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
