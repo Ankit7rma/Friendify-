f9b5421ce47e2ce1b5dbb60598f560157e0adc3a
Ankit Sharma <ankitsharma290101@gmail.com>
2024-01-02 21:43:18 +0530

    ShadCN Components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { Button } from "@/components/ui/button";
+
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div>
+      RootLayout
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default RootLayout;
